b4bb11b308ed2f6b9b16699362bcb3e6e3337a85
stephyg314 <georges@slu.edu>
2019-06-26 22:51:33 -0400

    adding code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Card from './Card/Card';
+import Button from './Button/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.updateCard = this.updateCard.bind(this);
+    this.state = {
+      cards: [
+        {id: 1, clue: "famous boxer", word: "ali", explanation: "he boxed"},
+        {id: 2, clue: "lake", word: "erie", explanation: "it's a lake ok"},
+        {id: 3, clue: "milks fave cookie", word: "oreo", explanation: "oreos go good with milk"},
+      ],
+      currentCard: {}
+      }
+    }
+    componentWillMount(){
+      const currentCards = this.state.cards;
+      this.setState({
+        cards: currentCards,
+        currentCard: this.getRandomCard(currentCards)
+      })
+    }
+    
+    getRandomCard(currentCards){
+      var card = currentCards[Math.floor(Math.random() * currentCards.length)]
+      return(card);
+  }
+  updateCard(){
+    const currentCards = this.state.cards;
+    this.setState({
+      currentCard: this.getRandomCard(currentCards)
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="cardSection">
+          <Card clue={this.state.currentCard.clue} 
+            word={this.state.currentCard.word} 
+            explanation={this.state.currentCard.explanation}
+          />
+        </div>
+        <div className="rightButtonSeciont">
+          <Button drawCard={this.updateCard}
+          />
+        </div>        
+        <div className="wrongButtonSeciont">
+          <Button drawCard={this.updateCard}
+          />
+        </div>         
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'; 
+import './Card.css'; 
+
+const Card = (props) => (
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="clue">{props.clue}</div>
+            </div>
+            <div className="back">
+                <div className="word">{props.word}</div>
+                <div className="explanation">{props.explanation}</div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Card 
